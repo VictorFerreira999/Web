8c42aca899871c0657b3a3d058eba3bbc0480cbe
Victor <victorferreira9990@gmail.com>
2024-01-07 19:12:46 -0300

    organização

--- conhecendo_javascript/index.js
+++ conhecendo_javascript/index.js
@@ -1,53 +1,55 @@
-var nome,sobrenome;
+// Declaração de variáveis
+var nome, sobrenome;
 var idade, peso;
-var ficha; // variavel tipo objeto
-var listaDeNomes; //variavel do tipo array
+var ficha; // variável tipo objeto
+var listaDeNomes; // variável do tipo array
 var listaDeCarros;
 
+// Atribuição de valores às variáveis
 nome = 'Victor';
 sobrenome = 'Ferreira';
 idade = 25;
 peso = 80;
 
+// Objeto 'ficha' contendo informações do usuário
 ficha = {
    nome,
    sobrenome,
    idade,
    peso,
    corDoCabelo: 'Preto'
+};
 
-}
-
+// Array 'listaDeNomes' com alguns nomes
 listaDeNomes = [
    'Joao', 'Marcos', 'Pedro', 'Luiz'
-]
+];
 
+// Array de objetos 'listaDeCarros' com informações de carros
 listaDeCarros = [
    {
       placa: '0WW22',
       modelo: 'Renegade'
-   },{
+   },
+   {
       placa: 'MFI54',
       modelo: 'FERRARI'  
-   },{
+   },
+   {
       placa: 'UUU054',
       modelo: 'Civic'
    }
-]
+];
 
-
-console.log('Nome do usuario: ', nome);
-console.log('Sobrenome do usuario: ', sobrenome);
-console.log('Idade do usuario: ', idade);
-console.log('A ficha do usuario é: ');
+// Saída no console para verificar os valores
+console.log('Nome do usuário: ', nome);
+console.log('Sobrenome do usuário: ', sobrenome);
+console.log('Idade do usuário: ', idade);
+console.log('A ficha do usuário é: ');
 console.log(ficha);
 console.log('***********************');
+console.log('Lista de nomes: ');
 console.log(listaDeNomes);
 console.log('***********************');
-console.log('***********************');
-console.log('Lista de carros')
+console.log('Lista de carros: ');
 console.log(listaDeCarros);
-
-
-
-
