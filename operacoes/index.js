94c9337562588f0b76dadd1c9fb19b6c92132159
Victor <victorferreira9990@gmail.com>
2024-01-07 17:43:02 -0300

    operações

---
+++ operacoes/index.js
@@ -0,0 +1,21 @@
+var resultado;
+var x = 2;
+var y = 5;
+
+resultado = x + y;
+console.log('A soma é: ', resultado);
+
+resultado = y - x;
+console.log('A subtração é: ', resultado);
+
+resultado = x * y;
+console.log('A multiplicação é: ', resultado);
+
+resultado = x / y;
+console.log('A divisão é: ', resultado);
+
+var peso = 80;
+var altura = 1.80;
+
+var imc = (peso / (altura * altura))
+console.log('O imc é: ', imc)
