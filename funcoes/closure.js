9a33833afaf827693d16c7a60316b39d78b5236e
Victor <victorferreira9990@gmail.com>
2024-01-16 09:31:10 -0300

    class e factory

---
+++ funcoes/closure.js
@@ -0,0 +1,14 @@
+// contexto léxico
+
+const x = 'Global'
+
+function fora() {
+   const x = 'Local'
+   function dentro() {
+      return x
+   }
+   return dentro
+}
+
+const minhaFuncao = fora()
+console.log(minhaFuncao())
