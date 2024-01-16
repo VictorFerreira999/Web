f2b0ee5619b08cff9318929556dabed9eaca54c7
Victor <victorferreira9990@gmail.com>
2024-01-16 08:05:34 -0300

    callBack e função construtora

---
+++ funcoes/tiposDeclaracao.js
@@ -0,0 +1,13 @@
+// function declaration
+
+function soma(x,y){
+   return x + y
+}
+
+//funtion expression
+const sub = function(x,y){
+   return x -y
+}
+
+// named function expresssion
+const mult = fun
