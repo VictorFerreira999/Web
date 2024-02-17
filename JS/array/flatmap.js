9f99ec0ed8b7cd7a8b220497587c06901f465f04
victor-ferreira <victorferreira9990@gmail.com>
2024-02-17 17:33:23 -0300

    flat map

---
+++ JS/array/flatmap.js
@@ -0,0 +1,24 @@
+const escola = [{
+   nome:'Turma N1',
+   alunos: [{
+      nome: 'Gustavo',
+      nota: 8.1
+   }, {
+      nome: 'Ana',
+      nota: 9.3
+   }]
+}, {
+   nome: 'Turma N2',
+   alunos: [{
+      nome:'Rebeca',
+      nota: 8.9
+   }, {
+      nome: 'Roberto',
+      nota: 7.3
+   }]
+}]
+
+const getNotaDoAluno = aluno => aluno.nota
+const getNotaDaTurma =  turma => turma.alunos.map(getNotaDoAluno)
+
+const notas1 = escola.map(getNotaDaTurma)
